b69ea3ede935b5dff69f0b5eba3d6c146a1e6d79
Iveta <quietbits@gmail.com>
2020-07-14 14:05:04 -0400

    Use functions for useEffect

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   rules: {
     "no-console": 0,
     "import/no-unresolved": "off",
-    "react-hooks/exhaustive-deps": "off",
     "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
   },
 };
