1e64dece1207e7afe5498083e0aae2ec4483e323
Alec Charbonneau <aleccharb21@gmail.com>
2020-07-29 10:17:37 -0700

    Receive modal + change keystore id to keyStoreId (#55)
    * add ReceiveTransaction component and functionality
    
    * change keystore id to keyStoreId
    
    * use qr code react package
    
    * name change, remove divs

---
+++ src/components/ReceiveTransaction.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { useRedux } from "hooks/useRedux";
+import QRCode from "qrcode.react";
+
+const El = styled.div`
+  text-align: center;
+  padding-bottom: 20px;
+`;
+
+const TempButtonEl = styled.button`
+  margin-bottom: 20px;
+`;
+
+export const ReceiveTransaction = () => {
+  const { account } = useRedux(["account"]);
+  const [isAccountIdCopied, setAccountIsIdCopied] = useState(false);
+  const accountId = account.data?.id;
+
+  return (
+    <El>
+      <El>
+        <h2>Your account QR code</h2>
+      </El>
+      <El>
+        Scan this QR code using a Stellar wallet app to make a payment to your
+        account
+      </El>
+      <El>
+        <QRCode value={accountId}></QRCode>
+      </El>
+      <El>{accountId}</El>
+      <CopyToClipboard
+        text={accountId}
+        onCopy={() => setAccountIsIdCopied(true)}
+      >
+        <TempButtonEl>
+          {isAccountIdCopied ? "Copied" : "Copy public key"}
+        </TempButtonEl>
+      </CopyToClipboard>
+    </El>
+  );
+};
