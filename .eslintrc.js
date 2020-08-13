df0657758962c5914a8917b01cfadb5f71a8dd05
Iveta <quietbits@gmail.com>
2020-08-13 13:34:34 -0400

    Upgrade wallet-sdk + eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,5 @@ module.exports = {
     "no-console": 0,
     "import/no-unresolved": "off",
     "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
-    // TODO: remove once @stellar/eslint-config is updated
-    "no-param-reassign": 0,
   },
 };
