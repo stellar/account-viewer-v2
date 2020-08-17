dea4c2e45e21e4fb35ec9788519130f6198d54d7
Iveta <quietbits@users.noreply.github.com>
2020-08-17 15:54:39 -0400

    Fonts + basic styles config (#80)
    * Fonts + basic styles config
    
    * Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createGlobalStyle } from "styled-components";
 
 import { store } from "config/store";
 import { Dashboard } from "pages/Dashboard";
 import { Landing } from "pages/Landing";
 import { NotFound } from "pages/NotFound";
+import { GlobalStyle } from "components/GlobalStyle";
 import { Network } from "components/Network";
 import { PrivateRoute } from "components/PrivateRoute";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: sans-serif;
-  }
-`;
 
 export const App = () => (
   <Provider store={store}>
---
+++ src/constants/styles.ts
@@ -0,0 +1,20 @@
+export enum PALETTE {
+  black = "#000000",
+  black80 = "#333333",
+  black60 = "#666666",
+  grey = "#CCCCCC",
+  lightGrey = "#E5E5E5",
+  white = "#FFFFFF",
+  purple = "#3E1BDB",
+}
+
+export enum FONT_FAMILY {
+  base = '"IBM Plex Sans", "Helvetica Neue", Arial, sans-serif',
+  monospace = '"IBM Plex Mono", Consolas, Menlo, monospace',
+}
+
+export enum FONT_WEIGHT {
+  light = 300,
+  normal = 400,
+  bold = 500,
+}
