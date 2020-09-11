1ab09cfa8970139849d6e7cfceeed877689dd207
Iveta <quietbits@gmail.com>
2020-09-11 16:41:06 -0400

    404 page styled + layout tweaks

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
-export const NotFound = () => (
-  <div>
-    <h1>Not Found</h1>
-  </div>
-);
+import { Button } from "components/basic/Button";
+import { Heading1 } from "components/basic/Heading";
+import { pageInsetStyle, PALETTE } from "constants/styles";
+
+const WrapperEl = styled.div`
+  ${pageInsetStyle};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+`;
+
+const MessageEl = styled.span`
+  font-size: 1rem;
+  line-height: 1.5rem;
+  margin-top: 1.5625rem;
+  margin-bottom: 3rem;
+`;
+
+const Heading1El = styled(Heading1)`
+  margin-bottom: 0;
+  text-align: center;
+`;
+
+const TestEl = styled.div`
+  color: ${PALETTE.purple};
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+  text-align: center;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+`;
+
+export const NotFound = () => {
+  const history = useHistory();
+
+  const handleBack = () => {
+    history.push({ pathname: "/", search: history.location.search });
+  };
+
+  return (
+    <WrapperEl>
+      <TestEl>Error 404</TestEl>
+      <Heading1El>Sorry, that page couldn’t be found.</Heading1El>
+      <MessageEl>Have you tried turning it off and on again?</MessageEl>
+      <Button onClick={handleBack}>Back to Account Viewer</Button>
+    </WrapperEl>
+  );
+};
