fdbca31ad810615bead5b5ec4c0e1b1791b7eaf4
Nando Vieira <me@fnando.com>
2020-11-24 16:56:11 -0800

    Add known accounts.
    Previously, this was being fetched straight from stellar.expert.

---
+++ src/constants/knownAccounts.ts
@@ -0,0 +1,231 @@
+type KnownAccount = { name: string; memoType: string; domain: string };
+
+export const knownAccounts: {
+  [publicKey: string]: KnownAccount;
+} = {
+  GASWJWFRYE55KC7MGANZMMRBK5NPXT3HMPDQ6SEXZN6ZPWYXVVYBFRTE: {
+    name: "AnchorUSD",
+    domain: "www.anchorusd.com",
+    memoType: "MEMO_ID",
+  },
+  GAHK7EEG2WWHVKDNT4CEQFZGKF2LGDSW2IVM4S5DP42RBW3K6BTODB4A: {
+    name: "Binance",
+    domain: "binance.com",
+    memoType: "MEMO_ID",
+  },
+  GCO2IP3MJNUOKS4PUDI4C7LGGMQDJGXG3COYX3WSB4HHNAHKYV5YL3VC: {
+    name: "Binance",
+    domain: "binance.com",
+    memoType: "MEMO_ID",
+  },
+  GAWPTHY6233GRWZZ7JXDMVXDUDCVQVVQ2SXCSTG3R3CNP5LQPDAHNBKL: {
+    name: "Bitfinex",
+    domain: "bitfinex.com",
+    memoType: "MEMO_ID",
+  },
+  GD4J347D3NT267QWCMJDZKJPEQFO2VENLZV6NUHFJQIDVMHGPWYRZ2BC: {
+    name: "BitGo",
+    domain: "bitgo.com",
+    memoType: "MEMO_ID",
+  },
+  GB6YPGW5JFMMP2QB2USQ33EUWTXVL4ZT5ITUNCY3YKVWOJPP57CANOF3: {
+    name: "Bittrex",
+    domain: "bittrex.com",
+    memoType: "MEMO_TEXT",
+  },
+  GARAR5QR7WRL24MQMSO4INWV7C5SE4EE2YVXTLD6ORONYFHSUAGZYSLN: {
+    name: "Blockchain.com",
+    domain: "blockchain.com",
+    memoType: "MEMO_ID",
+  },
+  GB7GRJ5DTE3AA2TCVHQS2LAD3D7NFG7YLTOEWEBVRNUUI2Q3TJ5UQIFM: {
+    name: "BTC38",
+    domain: "btc38.com",
+    memoType: "MEMO_ID",
+  },
+  GB3RMPTL47E4ULVANHBNCXSXM2ZA5JFY5ISDRERPCXNJUDEO73QFZUNK: {
+    name: "CEX.IO",
+    domain: "cex.io",
+    memoType: "MEMO_ID",
+  },
+  GBV4ZDEPNQ2FKSPKGJP2YKDAIZWQ2XKRQD4V4ACH3TCTFY6KPY3OAVS7: {
+    name: "Changelly",
+    domain: "changelly.com",
+    memoType: "MEMO_ID",
+  },
+  GDQP2KPQGKIHYJGXNUIYOMHARUARCA7DJT5FO2FFOOKY3B2WSQHG4W37: {
+    name: "Coinbase",
+    domain: "coinbase.com",
+    memoType: "MEMO_ID",
+  },
+  GDMXNQBJMS3FYI4PFSYCCB4XODQMNMTKPQ5HIKOUWBOWJ2P3CF6WASBE: {
+    name: "Cryptomkt",
+    domain: "www.cryptomkt.com",
+    memoType: "MEMO_ID",
+  },
+  GDLJS5SWQQOCB2B2HFCZIMRSAHLPJS427AG2UBOSUMG6TOCBOE5ZTMA5: {
+    name: "EXMO",
+    domain: "exmo.com",
+    memoType: "MEMO_ID",
+  },
+  GBE27W2DJZS4AFFR2HVZBK4KHD4TQQ4ITB2AQTYA5L57K6ZTRWPJUDH6: {
+    name: "GoodX",
+    domain: "goodx.network",
+    memoType: "MEMO_ID",
+  },
+  GCXDR4QZ4OTVX6433DPTXELCSEWQ4E5BIPVRRJMUR6M3NT4JCVIDALZO: {
+    name: "GOPAX",
+    domain: "gopax.com",
+    memoType: "MEMO_ID",
+  },
+  GC4KAS6W2YCGJGLP633A6F6AKTCV4WSLMTMIQRSEQE5QRRVKSX7THV6S: {
+    name: "IndoDax",
+    domain: "indodax.com",
+    memoType: "MEMO_TEXT",
+  },
+  GCNSGHUCG5VMGLT5RIYYZSO7VQULQKAJ62QA33DBC5PPBSO57LFWVV6P: {
+    name: "InterstellarExchange",
+    domain: "interstellar.exchange",
+    memoType: "MEMO_ID",
+  },
+  GA5XIGA5C7QTPTWXQHY6MCJRMTRZDOSHR6EFIBNDQTCQHG262N4GGKTM: {
+    name: "Kraken",
+    domain: "kraken.com",
+    memoType: "MEMO_ID",
+  },
+  GBJNV2MQA7M5GNBRDFW46JLXIN7ZLYVVM4UW4CWDZO4KZKXIXCRYHMH2: {
+    name: "KuCoin",
+    domain: "www.kucoin.com",
+    memoType: "MEMO_ID",
+  },
+  GBGII2C7M4TOEC2MVAZYG3TRFM3ATCCEWANSN4Q3AHEX3NRKXJCVZDEV: {
+    name: "OKEX",
+    domain: "okex.com",
+    memoType: "MEMO_ID",
+  },
+  GAEGOS7I6TYZSVHPSN76RSPIVL3SELATXZWLFO4DIAFYJF7MPAHFE7H4: {
+    name: "Papaya",
+    domain: "apay.io",
+    memoType: "MEMO_ID",
+  },
+  GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR: {
+    name: "Papaya",
+    domain: "apay.io",
+    memoType: "MEMO_ID",
+  },
+  GBR3RS2Z277FER476OFHFXQJRKYSQX4Z7XNWO65AN3QPRUANUASANG3L: {
+    name: "PapayaBot",
+    domain: "papayabot.com",
+    memoType: "MEMO_ID",
+  },
+  GBGVRE5DH6HGNYNLWQITKRQYGR4PWQEH6MOE5ELPY3I4XJPTZ7CVT4YW: {
+    name: "PapayaSwap",
+    domain: "papayabot.com",
+    memoType: "MEMO_ID",
+  },
+  GCF7F72LNF3ODSJIIWPJWEVWX33VT2SVZSUQ5NMDKDLK3N2NFCUAUHPT: {
+    name: "Poloniex",
+    domain: "poloniex.com",
+    memoType: "MEMO_ID",
+  },
+  GCLDH6L6FBLTD3H3B23D6TIFVVTFBLZMNBC3ZOI6FGI5GPQROL4FOXIN: {
+    name: "RippleFox",
+    domain: "ripplefox.com",
+    memoType: "MEMO_ID",
+  },
+  GBVOL67TMUQBGL4TZYNMY3ZQ5WGQYFPFD5VJRWXR72VA33VFNL225PL5: {
+    name: "Stellarport",
+    domain: "stellarport.io",
+    memoType: "MEMO_ID",
+  },
+  GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S: {
+    name: "Tempo",
+    domain: "tempocrypto.com",
+    memoType: "MEMO_ID",
+  },
+  GAW4E6NGM4NPNX2LO2BKDPCCTUX3FJLKWHPU4VQPGBIBQGD6JTVF5C7C: {
+    name: "Upbit",
+    domain: "upbit.com",
+    memoType: "MEMO_ID",
+  },
+  GDNQAK7B3WQKMEGKJK6FCDKIMKHOGYR7E637GCLFHWJOPCYE6VBALYDF: {
+    name: "WhiteStandard",
+    domain: "thewwallet.com",
+    memoType: "MEMO_ID",
+  },
+  GASBLVHS5FOABSDNW5SPPH3QRJYXY5JHA2AOA2QHH2FJLZBRXSG4SWXT: {
+    name: "Wirex",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GAESQGK5TTKPT2JY4STRN6MJU56LNHQVBFROGX5GFIWUPK3JHZ5F5FCI: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GAR6LML2TJ43TRHGG4CZZTASXZ4PWECJQUHNMS3OYX6SCKJ2T6F6WR5Q: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GAX5KN5SZKIJFSJJ6X2ARE3PUBX63LBGU5FG7SYS4FDM3YALCZUAEBUH: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GB6UZI3NWBU66O3VPOEVKPIUA7LL23A42KBOXSUIM75T6RJDBMWZXT34: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GBCAZ6C5UP5G5DVIMYJCFL3OCSM5KJPN6GQJCB45EBWCNPMQX2CZL5YC: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GBI7IEDTM6EDNURWNLBCU27MVP45OVJE7LRD4PFS67NHTTRKDZQGFLKN: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GBOFQCVZF2DSFLJPJMKH7CIECCN7TYVAT2CSD73IVEBJJQU2VW6JNFHT: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GBPJ35BBTUOXNSGUTB6TQQZWUA7EFZS55KY7Y5Z75TVTN7SPNJKXMPYI: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GC5ISVO2RW6WOOVCBXDJRTFHI464VM3OAODA2EQIZDL7L2ZETXLRJHCI: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GCCMMUJ4DNVPDJ7ABDD7JA3KXDN2ISCFPSEXIXXGLXKT3HGXV6PPIQUG: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GCPVAKXS3VTLE3HPF7B2Y54J3NOVUGTJYGENP3ZNM2MIVKHLZGK22IVQ: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GDC7UWGW676LQFJLEVWVOYOKFGVTPDQ4LVP27QS3W45XDDT4UQHWHHO4: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GDMEBQP7NSJBDFGRC4I6HRJODHLLJ6AWZ34YAMY5VIP2BM2QV6OIQZG3: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+  GDOXP56C4SBKZM7NIURUANA63NXTRMJUEOWVR6Q5KJSBGIO7UAWEBSEP: {
+    name: "WireX",
+    domain: "wirexapp.com",
+    memoType: "MEMO_ID",
+  },
+};
