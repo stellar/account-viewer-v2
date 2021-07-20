3d1700581399a4035f62712084a9464b17ec9544
Iveta <quietbits@gmail.com>
2021-07-20 19:30:42 -0400

    Dark mode: dashboard header in progress

--- src/components/CopyWithTooltip/index.tsx
+++ src/components/CopyWithTooltip/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
+import { Icon } from "@stellar/design-system";
 import "./styles.scss";
 
 enum TooltipPosition {
@@ -15,6 +16,7 @@ interface CopyWithTooltipProps {
   copyText: string;
   tooltipLabel?: string;
   tooltipPosition?: TooltipPosition;
+  showCopyIcon?: boolean;
   children: React.ReactNode;
 }
 
@@ -23,6 +25,7 @@ export const CopyWithTooltip: React.FC<CopyWithTooltipProps> &
   copyText,
   tooltipLabel = "Copied",
   tooltipPosition = TooltipPosition.bottom,
+  showCopyIcon,
   children,
 }) => {
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
@@ -46,11 +49,18 @@ export const CopyWithTooltip: React.FC<CopyWithTooltipProps> &
 
   return (
     <div className="CopyWithTooltip">
-      <div className="CopyWithTooltip__content">
-        <CopyToClipboard text={copyText} onCopy={handleCopyDone}>
+      <CopyToClipboard text={copyText} onCopy={handleCopyDone}>
+        <div title="Copy" role="button" className="CopyWithTooltip__content">
           {children}
-        </CopyToClipboard>
-      </div>
+
+          {showCopyIcon ? (
+            <div className="CopyWithTooltip__content__copy-icon">
+              <Icon.Copy />
+            </div>
+          ) : null}
+        </div>
+      </CopyToClipboard>
+
       <div
         className={`Tooltip CopyWithTooltip__tooltip CopyWithTooltip__tooltip--${tooltipPosition}`}
         style={customStyle}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,90 +1,12 @@
-import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { Layout, TextLink } from "@stellar/design-system";
+import { Layout, TextLink, Identicon } from "@stellar/design-system";
 
-import { ReactComponent as IconCopy } from "assets/svg/icon-copy.svg";
 import { resetStoreAction } from "config/store";
-import { Avatar } from "components/Avatar";
 import { CopyWithTooltip } from "components/CopyWithTooltip";
-import {
-  FONT_WEIGHT,
-  HEADER_HEIGHT_REM,
-  MEDIA_QUERIES,
-  PALETTE,
-} from "constants/styles";
 import { stopAccountWatcherAction } from "ducks/account";
 import { stopTxHistoryWatcherAction } from "ducks/txHistory";
-import { getFormattedPublicKey } from "helpers/getFormattedPublicKey";
 import { useRedux } from "hooks/useRedux";
-
-const BaseEl = styled.div`
-  height: ${HEADER_HEIGHT_REM}rem;
-  display: flex;
-  align-items: center;
-`;
-
-const AccountWrapperEl = styled(BaseEl)`
-  justify-content: center;
-  order: 1;
-  width: 100%;
-  margin-top: 1.2rem;
-
-  @media (${MEDIA_QUERIES.headerFooterHeight}) {
-    width: auto;
-    order: 0;
-    flex: 1;
-    justify-content: flex-end;
-    margin-top: 0;
-  }
-`;
-
-const SignOutWrapperEl = styled(BaseEl)``;
-
-const CopyPublicKeyButtonEl = styled.div`
-  display: flex;
-  position: relative;
-  align-items: center;
-  margin-left: 0.75rem;
-  margin-right: 0;
-  margin-top: 0.25rem;
-  font-size: 1rem;
-  line-height: 1.75rem;
-  font-weight: ${FONT_WEIGHT.medium};
-  color: ${PALETTE.black};
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  svg {
-    fill: ${PALETTE.purple};
-    height: 1.25rem;
-    width: 1.25rem;
-    margin-left: 0.75rem;
-    margin-top: -0.25rem;
-  }
-
-  &::after {
-    content: "";
-    cursor: default;
-    display: none;
-    width: 1px;
-    height: 2rem;
-    background-color: ${PALETTE.grey};
-    position: absolute;
-    top: -0.2rem;
-    right: -1.5rem;
-  }
-
-  @media (${MEDIA_QUERIES.headerFooterHeight}) {
-    margin-right: 2.6rem;
-
-    &::after {
-      display: block;
-    }
-  }
-`;
 
 export const Header = () => {
   const dispatch = useDispatch();
@@ -109,20 +31,15 @@ export const Header = () => {
     >
       {isAuthenticated && account.data && (
         <>
-          <AccountWrapperEl>
-            <Avatar publicAddress={account.data.id} />
-            <CopyWithTooltip copyText={account.data.id}>
-              <CopyPublicKeyButtonEl>
-                {getFormattedPublicKey(account.data.id)}
-                <IconCopy />
-              </CopyPublicKeyButtonEl>
+          <div className="Header__account">
+            <CopyWithTooltip copyText={account.data.id} showCopyIcon>
+              <Identicon publicAddress={account.data.id} shortenAddress />
             </CopyWithTooltip>
-          </AccountWrapperEl>
-          <SignOutWrapperEl>
-            <TextLink role="button" onClick={handleSignOut}>
-              Sign out
-            </TextLink>
-          </SignOutWrapperEl>
+          </div>
+
+          <TextLink role="button" onClick={handleSignOut}>
+            Sign out
+          </TextLink>
         </>
       )}
     </Layout.Header>
